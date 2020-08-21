85f70bd34a6c229b99e823f608498fd26fac4fe5
sophieO1970 <sophieolondie@gmail.com>
2020-08-22 01:36:39 +0300

    edited index.html and styles.css

--- js/script.js
+++ js/script.js
@@ -4,12 +4,7 @@ var akanFemaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
 function getAkanName() {
     var gender = document.getElementById ("gender").value;
     var dateOfBirth = document.getElementById ("date").value;
-    var DD = parseInt(dateOfBirth.substr(8,9));
-    var CC = parseInt(dateOfBirth.substr(0,2));
-    var MM = parseInt(dateOfBirth.substr(5,6));
-    var YY = parseInt(dateOfBirth.substr(1,3));
-    var day= parseInt(
-        (CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7
-    )
     
+    alert(gender)
+    alert(dateOfBirth)
 };
