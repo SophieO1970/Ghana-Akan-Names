677b36b44eb0a2e5b52cd1a63352b06dfc134b0c
sophieO1970 <sophieolondie@gmail.com>
2020-08-21 23:24:26 +0300

    added a function

--- js/script.js
+++ js/script.js
@@ -1,2 +1,8 @@
 var akanMaleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Yaw", "kofi", "Kwame"]
 var akanFemaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+
+function getAkanName() {
+    var gender = document.getElementById ("gender").value;
+    var dateOfBirth = document.getElementById ("date").value;
+    
+}
