b286ee624cdb07b1d32b00652c3be206ee259892
sophieO1970 <sophieolondie@gmail.com>
2020-08-23 00:57:31 +0300

    adds a general formular that finds specific day of the week

--- js/script.js
+++ js/script.js
@@ -9,4 +9,6 @@ function akanName() {
     var MM = parseInt(ydate.substr(5, 6));
     var YY = parseInt(ydate.substr(1, 3));
     var CC = parseInt(ydate.substr(0, 2));
+    var d = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7)
+
 }
