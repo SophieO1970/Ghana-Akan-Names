d08fcc27eb51f6afd42be84851ccc6fd544679a0
sophieO1970 <sophieolondie@gmail.com>
2020-08-22 21:26:12 +0300

    added days of the week with a promppt

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 var akanMaleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Yaw", "kofi", "Kwame"]
 var akanFemaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
+var weekDay = ["Sunday" , "Monday" , "Tuesday" , "Wednsday" , "Thursday" , "Friday" , "Saturday"]
 
-function getAkanName() {
-    var gender = document.getElementById ("gender").value;
-    var dateOfBirth = document.getElementById ("date").value;
+var dateOfBirth = prompt('please enter birth date');
+var monthOfBirth = prompt('please enter birth month');
+var yearOfBirth = prompt('please enter birth year');
+var genderAtBirth = prompt('please enter birth gender');
+alert('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function getAkanName() {
+//     var gender = document.getElementById ("gender").value;
+//     var dateOfBirth = document.getElementById ("date").value;
     
-    alert(gender)
-    alert(dateOfBirth)
+//     alert(gender)
+//     alert(dateOfBirth)
+//     alert(akanName)
 };
