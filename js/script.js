f2e215f173f19849da98c937071d253add46a09f
sophieO1970 <sophieolondie@gmail.com>
2020-08-23 00:45:00 +0300

    adds function that fetch data from html form

--- js/script.js
+++ js/script.js
@@ -1,30 +1,6 @@
 var akanMaleNames =["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Yaw", "kofi", "Kwame"]
 var akanFemaleNames =["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"]
-var weekDay = ["Sunday" , "Monday" , "Tuesday" , "Wednsday" , "Thursday" , "Friday" , "Saturday"]
 
-var dateOfBirth = prompt('please enter birth date');
-var monthOfBirth = prompt('please enter birth month');
-var yearOfBirth = prompt('please enter birth year');
-var genderAtBirth = prompt('please enter birth gender');
-alert('My Birthday is ' + [dateOfBirth] + '/' + [monthOfBirth] + '/' + [yearOfBirth]);
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function getAkanName() {
-//     var gender = document.getElementById ("gender").value;
-//     var dateOfBirth = document.getElementById ("date").value;
-    
-//     alert(gender)
-//     alert(dateOfBirth)
-//     alert(akanName)
-};
+function akanName() {
+    var gender = document.getElementById("gender").value;
+    var ydate = document.getElementById("ydate").value;
