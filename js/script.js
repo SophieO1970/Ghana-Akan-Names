d2c62eb43a9d35d11960c92ffefc2a679ad59873
sophieO1970 <sophieolondie@gmail.com>
2020-08-23 01:01:52 +0300

    adds a function that checks the day entered is a valid date, valid month equivalent name for a male or female

--- js/script.js
+++ js/script.js
@@ -10,5 +10,13 @@ function akanName() {
     var YY = parseInt(ydate.substr(1, 3));
     var CC = parseInt(ydate.substr(0, 2));
     var d = parseInt((CC / 4 - 2 * CC - 1 + (5 * YY) / 4 + (26 * (MM + 1)) / 10 + DD) % 7)
+    if (DD <= 0 || DD > 31) {
+        alert("Invalid day. Please enter a valid date")
+      } else if (MM <= 0 || MM > 12) {
+        alert("Invalid Month. Please enter a valid month")
+      } else if (gender === "Male") {
+        alert("Your Ghanian equivalent name is " + akanMaleNames[d])
+      } else
+        alert("Your Ghanian equivalent name is " + akanFemaleNames[d])
 
 }
